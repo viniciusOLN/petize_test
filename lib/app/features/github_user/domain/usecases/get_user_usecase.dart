edb1a7e4d45efa6ae7dba8e88e869b906c36a20f
vinicius <vinicius853o@gmail.com>
2022-12-05 19:33:28 -0300

    add: adding usecase to get a user by nickname

---
+++ lib/app/features/github_user/domain/usecases/get_user_usecase.dart
@@ -0,0 +1,14 @@
+import 'package:dartz/dartz.dart';
+import 'package:petize_test/app/core/error/failures.dart';
+import 'package:petize_test/app/features/github_user/domain/entitites/user.dart';
+import 'package:petize_test/app/features/github_user/domain/repositories/user_repository.dart';
+
+class GetUserUsecase {
+  final UserDomainRepository repository;
+
+  GetUserUsecase(this.repository);
+
+  Future<Either<Failure, User>> getUser(String username) async {
+    return await repository.getUserByUsername(username);
+  }
+}
