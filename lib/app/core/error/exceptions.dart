37c12b417f56dca31ad8fa33610801366b069922
vinicius <vinicius853o@gmail.com>
2022-12-05 23:20:59 -0300

    fix: handle exceptions from server

--- lib/app/core/error/exceptions.dart
+++ lib/app/core/error/exceptions.dart
@@ -1,5 +1,23 @@
 // server exceptions
 
-class NotFoundException implements Exception {}
+import 'package:equatable/equatable.dart';
 
-class GenericException implements Exception {}
+class NotFoundException extends Equatable implements Exception {
+  @override
+  List<Object?> get props => [];
+}
+
+class GenericException extends Equatable implements Exception {
+  @override
+  List<Object?> get props => [];
+}
+
+class Exceptions {
+  static handleException(int statusCode) {
+    if (statusCode == 404) {
+      return NotFoundException();
+    }
+
+    return GenericException();
+  }
+}
