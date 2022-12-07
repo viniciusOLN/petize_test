41426b77c5adc3f950c01427bb6010dad0857f67
vinicius <vinicius853o@gmail.com>
2022-12-07 14:45:33 -0300

    add: new repository

--- lib/app/features/github_user/data/repositories/get_user_repository_impl.dart
+++ lib/app/features/github_user/data/repositories/get_user_repository_impl.dart
@@ -1,5 +1,6 @@
 import 'package:petize_test/app/core/error/exceptions.dart';
 import 'package:petize_test/app/features/github_user/data/datasources/get_user_datasource.dart';
+import 'package:petize_test/app/features/github_user/data/models/user_model.dart';
 import 'package:petize_test/app/features/github_user/domain/entitites/user.dart';
 import 'package:petize_test/app/core/error/failures.dart';
 import 'package:dartz/dartz.dart';
@@ -13,11 +14,12 @@ class GetUserRepositoryImpl implements UserDomainRepository {
   @override
   Future<Either<Failure, User>> getUserByUsername(String username) async {
     try {
-      final userModel = await datasource.getUserByUsername(username);
+      UserModel userModel = await datasource.getUserByUsername(username);
       return Right(userModel);
-    } on NotFoundException {
-      return Left(NotFoundFailure());
     } catch (e) {
+      if (e is NotFoundException) {
+        return Left(NotFoundFailure());
+      }
       return Left(GenericFailure());
     }
   }
